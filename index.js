69a6953997bc8d64092c10f42adf64fa8b15ac09
jesandante007 <jesandante007@gmail.com>
2023-05-02 01:33:29 +0600

    data filtered and send dynamically

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get('/chefs', (req, res) => {
-  res.send(data)
-})
+app.get("/chefs", (req, res) => {
+  res.send(data);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = data.find((chef) => chef.id == id);
+  res.send(selectedChef);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});

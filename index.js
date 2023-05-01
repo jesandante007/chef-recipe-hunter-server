350edf6aa99f2f4cba47db7fe9aed1f8f2f2e6a6
jesandante007 <jesandante007@gmail.com>
2023-05-02 01:26:18 +0600

    data send success

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get('/chefs', (req, res) => {
+  res.send(data)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
